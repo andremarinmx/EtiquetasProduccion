f27efc89ab3c59d65fb00e702abdb0b628f7a77d
andremarinmx <andre707.am@gmail.com>
2022-09-03 12:32:08 -0600

    Crear órdenes

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,46 @@
+﻿using EtiquetasProduccion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EtiquetasProduccion.Controllers
+{
+    public class OrdersController : Controller
+    {
+        [HttpGet]
+        public ActionResult CrearOrden()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearOrden(
+            string OrderNumber,
+            float Height,
+            float Width,
+            int Line,
+            int PcsOrder,
+            string Address,
+            string Customer
+        )
+        {
+            using (EtiquetasProduccionContext db = new EtiquetasProduccionContext())
+            {
+                Order_ orden = new Order_();
+                orden.OrderNumber = OrderNumber;
+                orden.Height = Height;
+                orden.Width = Width;
+                orden.Line_ = Line;
+                orden.Pcs_Order = PcsOrder;
+                orden.Address_ = Address;
+                orden.Customer = Customer;
+                orden.Pcs_Box = 0;
+                db.Order_.Add(orden);
+                db.SaveChanges();
+                return View();
+            }
+        }
+    }
+}
